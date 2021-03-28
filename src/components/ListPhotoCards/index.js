5d7f6b4dd8974b8e5c456289dfbbe96d4961655c
eddev <edwardbrian96@gmail.com>
2021-03-28 08:32:41 -0500

    implementacion de graphql

--- src/components/ListPhotoCards/index.js
+++ src/components/ListPhotoCards/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-export const ListOfPhotoCard = () => {
+
+const withPhortis = graphql(gql`
+query getPhotos{
+    photos{
+      id,
+      categoryId,
+      src,
+      likes,
+      userId,
+      liked
+    }
+  }
+`)
+
+
+
+export const ListOfPhotoCardComponent = ({ data: { photos = [] } = {} }) => {
     return (
         <ul>
             {
-                [1, 2, 3].map(id =>
-                    <PhotoCard key={id} id={id} />
+                photos.map(item =>
+                    <PhotoCard
+                        key={item.id}
+                        {...item} />
                 )
             }
         </ul>
     )
 }
+
+export const ListOfPhotoCard = withPhortis(ListOfPhotoCardComponent)
